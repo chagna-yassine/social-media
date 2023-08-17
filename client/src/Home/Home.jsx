339ba4fbef0e4d5bb75295d413ae964b1e069611
Badeeeeer <142109522+silentDvoice@users.noreply.github.com>
2023-08-17 16:38:11 +0000

    Update Home.jsx

--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -3,6 +3,10 @@ import './Home.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useCookies } from 'react-cookie'
 import { handleBgImgs } from '../HandleBgImgs/handleBgImgs'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faComment, faHeart, faMessage, faPaperPlane, faSquarePlus, faUser  } from '@fortawesome/free-regular-svg-icons';
+import testImg from "../Images/Light-5.jpeg"
+import { faHouse, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const Home = () => {
 
@@ -17,84 +21,99 @@ const Home = () => {
   return (
     <div id='Main' className='Main'>
         <img id='Main-img' loading='lazy' src='' alt="Signup" />
-        <div className="Main-container">
+        <div className={`Main-container ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
             <div className={`Feed-container ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
             <ul className={`list-group List ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
-              <li className={`list-group-item List ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
+              <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
                 <div className={`card Post ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
-                  <div className="card-body">
-                    <div className={`card mb-3 Post ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
+                  <div className="card-body Post-header">
+                    <div className={`card border-0 mb-3 Post-info ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
                       <div className="row g-0">
-                        <div className="col-md-4">
+                        <div className="Logo-container">
                           <div className={`Logo ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}></div>
                         </div>
-                        <div className="col-md-8">
+                        <div className="col-md-8 d-flex align-items-center">
                           <h2 className={`Label ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>User Name</h2>
                         </div>
                       </div>
                     </div>
                   </div>
                   <div className={`card Post-content ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
-                    <div className="card-body">
-                    <p className={`Label ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>Label</p>
-                    </div>
-                    <img src="../Images/Light-5.jpeg" className="card-img-top" alt="Post" />
+                    <img src={testImg} className="card-img-top Post-content-media" alt="Post" />
                   </div>
-                  <ul className="list-group">
-                    <li className={`list-group-item More ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>A second item</li>
-                  </ul>
+                    <div className={`list-group-item Interactions ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
+                       <div className="Interactions-item">
+                          <FontAwesomeIcon className="Interactions-item-icon" icon={faHeart}/>
+                       </div>
+                       <div className="Interactions-item">
+                          <FontAwesomeIcon className="Interactions-item-icon" icon={faComment}/>
+                       </div>
+                       <div className="Interactions-item">
+                          <FontAwesomeIcon className="Interactions-item-icon" icon={faPaperPlane}/>
+                       </div>
+                    </div>
                 </div>
               </li>
 
-              <li className={`list-group-item List ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
+              <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
                 <div className={`card Post ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
-                  <div className="card-body">
-                    <div className={`card mb-3 Post ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
+                  <div className="card-body Post-header">
+                    <div className={`card border-0 mb-3 Post-info ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
                       <div className="row g-0">
-                        <div className="col-md-4">
+                        <div className="Logo-container">
                           <div className={`Logo ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}></div>
                         </div>
-                        <div className="col-md-8">
+                        <div className="col-md-8 d-flex align-items-center">
                           <h2 className={`Label ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>User Name</h2>
                         </div>
                       </div>
                     </div>
                   </div>
                   <div className={`card Post-content ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
-                    <div className="card-body">
-                    <p className={`Label ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>Label</p>
-                    </div>
-                    <img src="../Images/Light-5.jpeg" className="card-img-top" alt="Post" />
+                    <img src={testImg} className="card-img-top Post-content-media" alt="Post" />
                   </div>
-                  <ul className="list-group">
-                    <li className={`list-group-item More ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>A second item</li>
-                  </ul>
+                    <div className={`list-group-item Interactions ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
+                       <div className="Interactions-item">
+                          <FontAwesomeIcon className="Interactions-item-icon" icon={faHeart}/>
+                       </div>
+                       <div className="Interactions-item">
+                          <FontAwesomeIcon className="Interactions-item-icon" icon={faComment}/>
+                       </div>
+                       <div className="Interactions-item">
+                          <FontAwesomeIcon className="Interactions-item-icon" icon={faPaperPlane}/>
+                       </div>
+                    </div>
                 </div>
               </li>
 
-              <li className={`list-group-item List ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
+              <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
                 <div className={`card Post ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
-                  <div className="card-body">
-                    <div className={`card mb-3 Post ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
+                  <div className="card-body Post-header">
+                    <div className={`card border-0 mb-3 Post-info ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
                       <div className="row g-0">
-                        <div className="col-md-4">
+                        <div className="Logo-container">
                           <div className={`Logo ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}></div>
                         </div>
-                        <div className="col-md-8">
+                        <div className="col-md-8 d-flex align-items-center">
                           <h2 className={`Label ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>User Name</h2>
                         </div>
                       </div>
                     </div>
                   </div>
                   <div className={`card Post-content ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
-                    <div className="card-body">
-                    <p className={`Label ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>Label</p>
-                    </div>
-                    <img src="../Images/Light-5.jpeg" className="card-img-top" alt="Post" />
+                    <img src={testImg} className="card-img-top Post-content-media" alt="Post" />
                   </div>
-                  <ul className="list-group">
-                    <li className={`list-group-item More ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>A second item</li>
-                  </ul>
+                    <div className={`list-group-item Interactions ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
+                       <div className="Interactions-item">
+                          <FontAwesomeIcon className="Interactions-item-icon" icon={faHeart}/>
+                       </div>
+                       <div className="Interactions-item">
+                          <FontAwesomeIcon className="Interactions-item-icon" icon={faComment}/>
+                       </div>
+                       <div className="Interactions-item">
+                          <FontAwesomeIcon className="Interactions-item-icon" icon={faPaperPlane}/>
+                       </div>
+                    </div>
                 </div>
               </li>
             </ul>
@@ -102,15 +121,43 @@ const Home = () => {
             </div>
             
             <div className={`Side-bar ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
-              <div className={`Logo ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}></div>
+              <div className="Side-bar-header">
+                  <div className="Side-bar-header-label"><h5>notifications</h5></div>
+                  <div className="Side-bar-header-icon-container">
+                      <FontAwesomeIcon className='Side-bar-header-icon' icon={faBell}/>
+                  </div>
+              </div>
               <ul className="list-group List">
-                <li className={`list-group-item List-Item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>A second item</li>
-                <li className={`list-group-item List-Item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>A third item</li>
-                <li className={`list-group-item List-Item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>A fourth item</li>
-                <li className={`list-group-item List-Item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>And a fifth one</li>
+                <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>User like your post</li>
+                <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>User comment on your post</li>
+                <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>User share your post</li>
+                <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>You have a message from User</li>
+                <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>User like your post</li>
+                <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>User comment on your post</li>
+                <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>User share your post</li>
+                <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>You have a message from User</li>
+                <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>User like your post</li>
+                <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>User comment on your post</li>
+                <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>User share your post</li>
+                <li className={`list-group-item List-item ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>You have a message from User</li>
               </ul>
             </div>
             <div className={`bottom-bar ${currentDisplayMode === 'dark' ? 'dark' : 'light'}`}>
+              <div className="bottom-bar-item">
+                <FontAwesomeIcon className='bottom-bar-item-icon' icon={faHouse}/>
+              </div>
+              <div className="bottom-bar-item">
+                <FontAwesomeIcon className='bottom-bar-item-icon' icon={faMagnifyingGlass}/>
+              </div>
+              <div className="bottom-bar-item">
+                <FontAwesomeIcon className='bottom-bar-item-icon' icon={faSquarePlus}/>
+                </div>
+              <div className="bottom-bar-item">
+                <FontAwesomeIcon className='bottom-bar-item-icon' icon={faMessage}/>
+              </div>
+              <div className="bottom-bar-item">
+                <FontAwesomeIcon className='bottom-bar-item-icon' icon={faUser}/>
+              </div>
             </div>
         </div>
     </div>
