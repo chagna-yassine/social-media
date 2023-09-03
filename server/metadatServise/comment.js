5e7ed26f93e9621a1a222534f7ca7efc7faae3f3
silentDvoice <badrslaoui8@gmail.com>
2023-09-03 00:07:19 +0000

    Add Comment Replies (Worked Only In Profile And Not 100%

--- server/metadatServise/comment.js
+++ server/metadatServise/comment.js
@@ -1,10 +1,20 @@
 import mongoose from "mongoose";
 
+const repliesSchema = new mongoose.Schema({
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    post_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Post'},
+    cmnt_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment'},
+    replied_to: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    text: {type: String},
+    createdAt: { type: Date, default: Date.now }
+  });
+
 const commentSchema = new mongoose.Schema({
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     post_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Post'},
     text: {type: String},
-    created_at: {type: Date, default: Date.now }
+    replies: [repliesSchema],
+    created_at: {type: Date, default: Date.now },
 });
 
 export const Comment = mongoose.model('comment', commentSchema);
